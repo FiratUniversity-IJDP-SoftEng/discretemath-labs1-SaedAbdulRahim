a8973980141f59903707215f823930c47ab12c5d
Saed Abdul Rahim <151928187+SaedAbdulRahim@users.noreply.github.com>
2025-11-19 22:18:13 +0300

    Generate and print 10 random 100-digit primes

--- ex-5.py
+++ ex-5.py
@@ -1,1 +1,12 @@
+import random
 
+def random_100_digit_prime():
+    while True:
+        num = random.randrange(10**99, 10**100)
+        if is_prime(num):
+            return num
+
+primes_100_digits = [random_100_digit_prime() for _ in range(10)]
+
+for p in primes_100_digits:
+    print(p, "\n")
